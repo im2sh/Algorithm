f7a9bae1489d4fb738a73721e80b24741d34e92d
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-03-10 17:36:01 +0900

    Using ASCII Code

--- 1/1 - F : 11655.cpp
+++ 1/1 - F : 11655.cpp
@@ -1,3 +1,25 @@
-//
-// Created by 이석환 on 2023/03/10.
-//
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    getline(cin, s);
+
+    for (int i = 0; i < s.size(); i++) {
+        if (65 <= s[i] && s[i] < 78)
+            s[i] = s[i] + 13;
+        else if (78 <= s[i] && s[i] < 91)
+            s[i] = s[i] - 13;
+        else if (97 <= s[i] && s[i] < 110)
+            s[i] = s[i] + 13;
+        else if (110 <= s[i] && s[i] < 123)
+            s[i] = s[i] - 13;
+        else
+            continue;
+    }
+    cout << s << "\n";
+}
