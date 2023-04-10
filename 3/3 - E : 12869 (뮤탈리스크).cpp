ff6bef90b9dbb7b874f9212807d2225bb6a39738
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-10 16:18:30 +0900

    BFS 최단거리 찾기 (DP로 구현 가능)

--- 3/3 - E : 12869 (뮤탈리스크).cpp
+++ 3/3 - E : 12869 (뮤탈리스크).cpp
@@ -1,52 +1,77 @@
 #include <iostream>
-#include <vector>
+#include <queue>
+#include <algorithm>
+
+#define SIZE 64
 
 using namespace std;
-
 int N;
-int cnt = 0;
-vector<int> scv;
 
-bool comp(int a, int b) {
-    return a > b;
+int visited[SIZE][SIZE][SIZE];
+int temp[3] = {0,};
+
+struct scv {
+    int a, b, c;
+};
+
+int attack[6][3]{
+        {9, 3, 1},
+        {9, 1, 3},
+        {3, 9, 1},
+        {3, 1, 9},
+        {1, 9, 3},
+        {1, 3, 9}
+};
+
+int solve(int a, int b, int c) {
+    queue<scv> q;
+    q.push({a, b, c});
+    visited[a][b][c] = 1;
+    while (q.size()) {
+        int x = q.front().a;
+        int y = q.front().b;
+        int z = q.front().c;
+        q.pop();
+        if (visited[0][0][0])
+            break;
+        for (int i = 0; i < 6; i++) {
+            int na = x - attack[i][0];
+            int nb = y - attack[i][1];
+            int nc = z - attack[i][2];
+
+            if (na < 0)
+                na = 0;
+            if (nb < 0)
+                nb = 0;
+            if (nc < 0)
+                nc = 0;
+//            int na = max(0, x - attack[i][0]);
+//            int nb = max(0, y - attack[i][1]);
+//            int nc = max(0, z - attack[i][2]);
+
+            if (visited[na][nb][nc])
+                continue;
+            visited[na][nb][nc] = visited[x][y][z] + 1;
+            q.push({na, nb, nc});
+        }
+    }
+    return visited[0][0][0] - 1;
+}
+
+void Init() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> temp[i];
+    }
+    fill(&visited[0][0][0], &visited[0][0][0] + SIZE * SIZE * SIZE, 0);
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int temp;
-    cin >> N;
-    scv.push_back(0);
-    scv.push_back(0);
-    scv.push_back(0);
-    for (int i = 0; i < N; i++) {
-        cin >> temp;
-        scv[i] = temp;
-    }
 
-    while (true) {
-        int check = 0;
-        for (int i = 0; i < N; i++) {
-            if (scv[i] <= 0) {
-                check++;
-            }
-        }
-        if (check == N) {
-            break;
-        }
-        if (scv.size() == 3) {
-            scv[0] = scv[0] - 9;
-            scv[1] = scv[1] - 3;
-            scv[2] = scv[2] - 1;
-        } else if (scv.size() == 2) {
-            scv[0] = scv[0] - 9;
-            scv[1] = scv[1] - 3;
-        } else
-            scv[0] = scv[0] - 9;
-
-        sort(scv.begin(), scv.end(), comp);
-        cnt++;
-    }
-    cout << cnt << "\n";
+    Init();
+    cout << solve(temp[0], temp[1], temp[2]) << "\n";
+    return 0;
 }
