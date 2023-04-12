00c9328220d77d1b82531e7ff683b96658394393
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-12 20:51:46 +0900

    Recursion

--- Algorithm_Study/3/3 - F : 16637 (괄호 추가하기).cpp
+++ Algorithm_Study/3/3 - F : 16637 (괄호 추가하기).cpp
@@ -7,7 +7,7 @@ using namespace std;
 int N;
 string S;
 
-vector<int> num;
+vector<int> numbers;
 vector<char> op;
 int ret = -987654321;
 
@@ -17,17 +17,17 @@ int calcu(char a, int b, int c) {
     if (a == '*') return b * c;
 }
 
-void solve(int here, int _num) {
-    //cout << here << ":" << _num << "\n";
-    if (here == num.size() - 1) {
-        ret = max(ret, _num);
+void solve(int here, int num) {
+    //cout << here << ":" << num << "\n";
+    if (here == numbers.size() - 1) {
+        ret = max(ret, num);
         return;
     }
-    solve(here + 1, calcu(op[here], _num, num[here + 1]));
+    solve(here + 1, calcu(op[here], num, numbers[here + 1]));
 
-    if (here + 2 <= num.size() - 1) {
-        int temp = calcu(op[here + 1], num[here + 1], num[here + 2]);
-        solve(here + 2, calcu(op[here], _num, temp));
+    if (here + 2 <= numbers.size() - 1) {
+        int temp = calcu(op[here + 1], numbers[here + 1], numbers[here + 2]);
+        solve(here + 2, calcu(op[here], num, temp));
     }
     return;
 }
@@ -38,7 +38,7 @@ void Init() {
 
     for (int i = 0; i < N; i++) {
         if (i % 2 == 0)
-            num.push_back(S[i] - '0');
+            numbers.push_back(S[i] - '0');
         else
             op.push_back(S[i]);
     }
@@ -50,7 +50,7 @@ int main(void) {
     cout.tie(NULL);
 
     Init();
-    solve(0, num[0]);
+    solve(0, numbers[0]);
     cout << ret << "\n";
     return 0;
 }
