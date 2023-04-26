9b665f456c8d3bf2ccd1e986ed033303388d9e86
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-27 01:30:55 +0900

    Stop and BFS

---
+++ Algorithm_Study/3/3 - I : 17071 (숨바꼭질5).cpp
@@ -0,0 +1,36 @@
+/*
+ * 보류
+ */
+
+#include <iostream>
+#include <queue>
+
+#define MAXMOVE 500004
+
+using namespace std;
+
+int N, K;
+int visited[MAXMOVE];
+int Kvisited[MAXMOVE];
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N >> K;
+}
+
+
+void solve() {
+
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
