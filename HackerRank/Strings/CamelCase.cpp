c426cfe6c08abb031c43ef66170c38081cf976d1
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-27 21:30:45 +0900

    대문자/소문자

--- HackerRank/Strings/CamelCase.cpp
+++ HackerRank/Strings/CamelCase.cpp
@@ -1,3 +1,32 @@
-//
-// Created by 이석환 on 2023/04/27.
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+int cnt;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+}
+
+void Init() {
+    cin >> s;
+}
+
+int solve() {
+    for (int i = 0; i < s.size(); i++) {
+        if (65 <= s[i] && s[i] <= 91)
+            cnt++;
+    }
+    return cnt + 1;
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    cout << solve();
+    return 0;
+}
--- HackerRank/Strings/Strong Password.cpp
+++ HackerRank/Strings/Strong Password.cpp
@@ -1,3 +1,87 @@
-//
-// Created by 이석환 on 2023/04/27.
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+string numbers = "0123456789";
+string lower_case = "abcdefghijklmnopqrstuvwxyz";
+string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string special_characters = "!@#$%^&*()-+";
+int N;
+string s;
+int cnt;
+bool check[4];
+int size1;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+}
+
+void Init() {
+    cin >> N;
+    cin >> s;
+}
+
+void calcu() {
+    for (int i = 0; i < 4; i++) {
+        if (!check[i]) {
+            cnt++;
+            size1++;
+        }
+    }
+
+    while (size1 < 6) {
+        size1++;
+        cnt++;
+    }
+};
+
+int solve() {
+    size1 = s.size();
+    for (int i = 0; i < s.size(); i++) {
+
+        for (int a = 0; a < numbers.size(); a++) {
+            if (s[i] == numbers[a]) {
+                check[0] = true;
+                break;
+            }
+        }
+
+        for (int b = 0; b < lower_case.size(); b++) {
+            if (s[i] == lower_case[b]) {
+                check[1] = true;
+                break;
+            }
+        }
+
+
+        for (int c = 0; c < upper_case.size(); c++) {
+            if (s[i] == upper_case[c]) {
+                check[2] = true;
+                break;
+            }
+        }
+
+
+        for (int d = 0; d < special_characters.size(); d++) {
+            if (s[i] == special_characters[d]) {
+                check[3] = true;
+                break;
+            }
+        }
+
+    }
+
+    calcu();
+
+    return cnt;
+}
+
+
+int main(void) {
+    FastIO();
+    Init();
+    cout << solve();
+    return 0;
+}
