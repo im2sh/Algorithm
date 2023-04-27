017c6cb69714b0666374c7882a5858fb14b65bae
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-28 02:16:51 +0900

    문자열 비교

--- HackerRank/Strings/Mars Exploration.cpp
+++ HackerRank/Strings/Mars Exploration.cpp
@@ -1,3 +1,36 @@
-//
-// Created by 이석환 on 2023/04/28.
-//
+#include <iostream>
+
+using namespace std;
+string s;
+string sos = "";
+int cnt;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> s;
+}
+
+void solve() {
+    int size = s.size();
+    int n = size / 3;
+    for (int i = 0; i < n; i++) {
+        sos += "SOS";
+    }
+    for (int i = 0; i < size; i++) {
+        if (s[i] != sos[i])
+            cnt++;
+    }
+    cout << cnt << "\n";
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
