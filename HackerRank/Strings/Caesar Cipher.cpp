2d0559b9656d3b69fde06513483c88b28e598c2f
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-04-28 02:09:52 +0900

    아스키 코드

--- HackerRank/Strings/Caesar Cipher.cpp
+++ HackerRank/Strings/Caesar Cipher.cpp
@@ -1,3 +1,47 @@
-//
-// Created by 이석환 on 2023/04/27.
-//
+#include <iostream>
+
+using namespace std;
+
+string original;
+string rotated;
+
+int N, K;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N;
+    cin >> original;
+    cin >> K;
+}
+
+void solve() {
+    int temp;
+    for (int i = 0; i < original.size(); i++) {
+        if (97 <= original[i] && original[i] <= 122)
+            temp = original[i] + K % 26;
+        else if (65 <= original[i] && original[i] <= 90)
+            temp = original[i] + K % 26;
+        else
+            temp = original[i];
+        if (temp > 122 && 97 <= original[i] && original[i] <= 122) {
+            temp -= 26;
+        }
+        if (temp > 90 && 65 <= original[i] && original[i] <= 90)
+            temp -= 26;
+        rotated += temp;
+    }
+    cout << rotated;
+}
+
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
