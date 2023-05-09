14a5883ed50c28b8079d02acca342d85a20b46ec
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-05-10 00:56:33 +0900

    BFS

---
+++ Algorithm_Study/3/3 - O : 15684 (사다리 조작).cpp
@@ -0,0 +1,27 @@
+//
+// Created by 이석환 on 2023/05/04.
+//
+#include <iostream>
+
+using namespace std;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+
+}
+
+void solve() {
+
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
