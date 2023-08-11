03705391243eb4fb6a3267154ee0c888ebf5f529
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-08-11 21:43:07 +0900

    다음 기회에

---
+++ Algorithm_Study/5/5 - R : 17825 (주사위 윷놀이).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+
+}
+
+void solve() {
+    cout << "나중에 풀래 ,,";
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
