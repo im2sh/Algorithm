dc52283737fac1c31872f19cf3de1cfdf6e19fa7
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-07 23:36:45 +0900

    Stack

--- Solve/13565 : 침투.cpp
+++ Solve/13565 : 침투.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int N, M;
+bool flag = false;
 int BOARD[1001][1001];
 int visited[1001][1001];
 const int dy[4] = {0, 0, -1, 1};
@@ -17,14 +18,14 @@ void FastIO() {
 
 void Init() {
     cin >> M >> N;
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            scanf("%1d", &BOARD[i][j]);
+    for (int y = 0; y < M; y++) {
+        for (int x = 0; x < N; x++) {
+            scanf("%1d", &BOARD[y][x]);
         }
     }
 }
 
-void BFS(int y, int x) {
+bool BFS(int y, int x) {
     queue<pair<int, int>> q;
     visited[y][x] = 1;
     q.push(make_pair(y, x));
@@ -38,28 +39,27 @@ void BFS(int y, int x) {
             int ny = cy + dy[dir];
             int nx = cx + dx[dir];
 
-            if (ny < 0 || nx < 0 || ny >= M || nx >= N)
+            if (ny < 0 || ny >= M || nx < 0 || nx >= N)
                 continue;
-            if (BOARD[ny][nx] == 0 && !visited[ny][nx]) {
-                visited[ny][nx] = true;
-                q.push(make_pair(ny, nx));
-            }
+            if (BOARD[ny][nx] == 1 || visited[ny][nx])
+                continue;
+            visited[ny][nx] = 1;
+            q.push({ny, nx});
         }
-
     }
+    for (int i = 0; i < N; i++) {
+        if (visited[M - 1][i])
+            return true;
+    }
+    return false;
 }
 
 void solve() {
-    for (int j = 0; j < N; j++) {
-        if (BOARD[0][j] == 0 && !visited[0][j]) {
-            BFS(0, j);
-        }
-    }
-
-    bool flag = false;
-    for (int j = 0; j < N; j++) {
-        if (visited[M - 1][j]) {
-            flag = true;
+    for (int i = 0; i < N; i++) {
+        if (BOARD[0][i] == 0 && !visited[0][i]) {
+            if (BFS(0, i)) {
+                flag = true;
+            }
         }
     }
 
