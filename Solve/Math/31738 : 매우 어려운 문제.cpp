d1ccd923e8bf5e55cf999264c3677850c059016e
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2024-09-30 23:40:59 +0900

    Math

---
+++ Solve/Math/31738 : 매우 어려운 문제.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+
+ll N, M, ret = 1;
+
+void FastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+void Init() {
+    cin >> N >> M;
+}
+
+void solve() {
+    if (N > M)
+        cout << 0;
+    else {
+        for (ll i = 2; i <= N; i++)
+            ret = (ret * i) % M;
+        cout << ret;
+    }
+}
+
+int main(void) {
+    FastIO();
+    Init();
+    solve();
+    return 0;
+}
